220bc71ccc01aa582e6c49c6e719b5c9e429e8ab
UmangiM <umangi.mistry@ibm.com>
2025-07-25 18:43:07 +0530

    Add files via upload

---
+++ Java/Activities/HashSetEx.java
@@ -0,0 +1,35 @@
+package Activities;
+import java.util.HashSet;
+
+public class HashSetEx {
+	public static void main(String[] args) {
+        // Create a HashSet
+        HashSet<String> hs = new HashSet<>();
+
+        hs.add("Apple");
+        hs.add("Banana");
+        hs.add("Cherry");
+        hs.add("Date");
+        hs.add("Papaya");
+        hs.add("Melon");
+
+        
+        System.out.println("Size of HashSet: " + hs.size());
+
+        
+        hs.remove("Cherry");
+        System.out.println("Removed 'Cherry' from the HashSet.");
+
+        
+        boolean isRemoved = hs.remove("Grapes");
+        System.out.println("Tried removing 'Grapes': " + (isRemoved ? "Success" : "Not found"));
+
+        
+        System.out.println("Contains 'Apple'? " + hs.contains("Apple"));
+        System.out.println("Contains 'Cherry'? " + hs.contains("Cherry"));
+
+        
+        System.out.println("Updated HashSet: " + hs);
+    }
+
+}
---
+++ Java/Activities/MapColors.java
@@ -0,0 +1,24 @@
+package Activities;
+import java.util.*;
+public class MapColors {
+	 public static void main(String[] args) {
+	        
+	        Map<Integer, String> colours = new HashMap<>();
+
+	        colours.put(1, "Red");
+	        colours.put(2, "Blue");
+	        colours.put(3, "Green");
+	        colours.put(4, "Yellow");
+	        colours.put(5, "Purple");
+
+	        System.out.println("Map Colours: " + colours);
+
+	        colours.remove(3);
+	        System.out.println("After removing key 3: " + colours);
+
+	        boolean hasGreen = colours.containsValue("Green");
+	        System.out.println("Does the map contain 'Green'? " + hasGreen);
+
+	        System.out.println("Size of the Map: " + colours.size());
+	    }
+}
