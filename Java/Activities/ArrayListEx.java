84b3a07bf31796814a5d3a1326909cc94b237c53
UmangiM <umangi.mistry@ibm.com>
2025-07-25 16:41:24 +0530

    Add files via upload

---
+++ Java/Activities/ArrayListEx.java
@@ -0,0 +1,42 @@
+package Activities;
+
+
+	import java.util.ArrayList;
+
+	public class ArrayListEx {
+	    public static void main(String[] args) {
+	       
+	        ArrayList<String> myList = new ArrayList<>();
+
+	        myList.add("Alpha");
+	        myList.add("Bita");
+	        myList.add("Gamma");
+	        myList.add("Delta");
+	       
+
+	        
+	        System.out.println("All names in the list:");
+	        for (int i = 0; i < myList.size(); i++) {
+	            System.out.println(myList.get(i));
+	        }
+
+	        
+	        String thirdName = myList.get(2); 
+	        System.out.println("\nThe 3rd name in the list is: " + thirdName);
+
+	        
+	        String nameToCheck = "Delta";
+	        boolean exists = myList.contains(nameToCheck);
+	        System.out.println("\nDoes the name \"" + nameToCheck + "\" exist in the list? " + exists);
+
+	       
+	        System.out.println("\nNumber of names in the list: " + myList.size());
+
+	      
+	        myList.remove("Bita");
+	        System.out.println("\nAfter removing 'Bita', the new number of names: " + myList.size());
+	    }
+	}
+
+
+
